b2c32771c7ae3d409f4fc11c644dc0122f1f829d
OswalDev <oswaldojsotog@gmail.com>
2022-04-30 21:52:50 -0400

    Bootstrap template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,10 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ asset('scss/navs/_global.css') }}" rel="stylesheet">
+    <link href="{{ asset('scss/navs/_sidebar.scss') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
